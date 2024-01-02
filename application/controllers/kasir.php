f1818d2ac362879f4114d1c4ca5d5158e7348c5d
yuliagustiyani <yuliagustiyani644@gmail.com>
2024-01-02 11:38:22 +0700

    first commit

---
+++ application/controllers/kasir.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kasir extends CI_Controller {
+
+    public function index() {
+        $this->load->view('kasir_view');
+    }
+
+    public function proses_pembayaran() {
+        $data['total'] = $this->input->post('total');
+        $data['bayar'] = $this->input->post('bayar');
+        $data['kembali'] = $data['bayar'] - $data['total'];
+
+        $this->load->view('kasir_view', $data);
+    }
+
+}
